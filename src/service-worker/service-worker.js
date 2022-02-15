6519c8f9deaebb0b2344419359224f695f031673
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2022-02-15 13:19:55 +0100

    sw

--- src/service-worker/service-worker.js
+++ src/service-worker/service-worker.js
@@ -1,1 +1,12 @@
-console.log('%c⚙ Hello from service worker ⚙', 'color: dodgerblue;font-size: 20px;font-weight: bold;');
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        console.log('%c⚙ Hello from service worker ⚙', 'color: dodgerblue;font-size: 20px;font-weight: bold;')
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+    );
+});
